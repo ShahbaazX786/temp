e9b7e0f7cc25a793ef4c5610a833b5735b006320
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-04-14 16:35:15 +0530

    Added project structure

--- frontend/portfolio/src/app/app.module.ts
+++ frontend/portfolio/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
+import { MyProjectsComponent } from './pages/my-projects/my-projects.component';
+import { MyBioComponent } from './pages/my-bio/my-bio.component';
+import { ResumeComponent } from './pages/resume/resume.component';
+import { LoginComponent } from './private/login/login.component';
+import { SignupComponent } from './private/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutMeComponent,
+    MyProjectsComponent,
+    MyBioComponent,
+    ResumeComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
